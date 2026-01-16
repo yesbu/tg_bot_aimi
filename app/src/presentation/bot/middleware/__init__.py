da78e81abc3fde399ab25587dff5d7f3f654b281
Jlumon4ek <nurymzhanserik@gmail.com>
2026-01-16 18:07:30 +0500

    fix init file

--- app/src/presentation/bot/middleware/__init__.py
+++ app/src/presentation/bot/middleware/__init__.py
@@ -1,10 +1,8 @@
 from .error_handler import ErrorHandlerMiddleware
 from .logging_middleware import LoggingMiddleware
-from .role_filter import RoleFilterMiddleware
 
 
 __all__ = [
     "ErrorHandlerMiddleware",
     "LoggingMiddleware",
-    "RoleFilterMiddleware",
 ]
