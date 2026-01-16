7153a8e688f1f40cd0def8f053140dd35c58fb69
Jlumon4ek <nurymzhanserik@gmail.com>
2026-01-16 17:45:53 +0500

    fix middleware

--- app/src/presentation/bot/middleware/role_filter.py
+++ app/src/presentation/bot/middleware/role_filter.py
@@ -1,10 +1,10 @@
 from typing import Callable, Dict, Any, Awaitable
 from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery, TelegramObject
-from dishka import FromDishka
 
-from src.application.interfaces.services import IUserService
 from src.domain.enums import Role
+from dishka.integrations.aiogram import CONTAINER_NAME
+from src.application.interfaces.services import IUserService
 
 
 class RoleFilterMiddleware(BaseMiddleware):
@@ -18,13 +18,19 @@ class RoleFilterMiddleware(BaseMiddleware):
         event: TelegramObject,
         data: Dict[str, Any]
     ) -> Any:
-        user_service: IUserService = data.get("user_service")
+
+        container = data.get(CONTAINER_NAME)
+        if not container:
+            return await handler(event, data)
         
         if isinstance(event, (Message, CallbackQuery)):
             telegram_id = event.from_user.id
-            user = await user_service.get_user_by_telegram_id(telegram_id)
             
-            if user and user.role in self.allowed_roles:
-                return await handler(event, data)
+            async with container() as request_container:
+                user_service = await request_container.get(IUserService)
+                user = await user_service.get_user_by_telegram_id(telegram_id)
+                
+                if user and user.role in self.allowed_roles:
+                    return await handler(event, data)
         
         return None
